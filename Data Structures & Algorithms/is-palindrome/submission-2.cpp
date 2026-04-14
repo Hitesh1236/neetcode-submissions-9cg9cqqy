ff17354454dbfb37b0180847df29d864f9b93d34
Hitesh1236 <hp12032006@gmail.com>
2026-04-14 09:45:38 +0530

    Add: is-palindrome - submission-2

---
+++ Data Structures & Algorithms/is-palindrome/submission-2.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        int l = 0;
+        int r = s.length() - 1;
+        while(l < r){
+            while(!isalnum(s[l])) l++;
+            while(!isalnum(s[r])) r--;
+            if(l >= r) return true;
+            if(tolower(s[l])==tolower(s[r])){
+                r--;
+                l++;
+            }
+            else return false;
+        }
+        return true;
+    }
+};
